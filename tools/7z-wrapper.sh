93b111f29d8a498011ded3614d23cb0b218abe01
2MuchCoff33 <65397884+2MuchC0ff33@users.noreply.github.com>
2026-02-28 06:03:03 +0800

    feat: add wrapper scripts for 7z, curl, and git to run from Cygwin

---
+++ tools/7z-wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Wrapper to run 7z installed via Scoop from Cygwin
+# Usage: ./7z-wrapper.sh <7z-args>
+/cygdrive/c/Users/galloa/scoop/shims/7z "$@"
---
+++ tools/git-wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Wrapper to run Git installed via Scoop from Cygwin
+# Usage: ./git-wrapper.sh <git-args>
+/cygdrive/c/Users/galloa/scoop/shims/git "$@"
