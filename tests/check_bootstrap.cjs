d65334a452b146f8797ede5c7f5dc328deaee014
2MuchCoff33 <65397884+2MuchC0ff33@users.noreply.github.com>
2026-02-28 13:23:04 +0800

    refactor: update paths and imports for CSS to Sass migration

--- tests/check_bootstrap.cjs
+++ tests/check_bootstrap.cjs
@@ -17,10 +17,10 @@ if (!fs.existsSync('vendor/autoload.php')) {
 namespace Dotenv;
 
 class Dotenv {
-    public_html_html static function createImmutable($dir) {
+    public_html static function createImmutable($dir) {
         return new self();
     }
-    public_html_html function load() {
+    public_html function load() {
         // mark that the phpdotenv branch executed
         putenv('DOTENV_USED=1');
         $_ENV['DOTENV_USED'] = '1';
